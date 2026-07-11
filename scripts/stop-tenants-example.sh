40aa9451847111398bc16838c7e7557fe2ee4a6c
Martín Mantovani <martin.mantovani@gmail.com>
2026-07-10 23:20:52 -0300

    fix

--- scripts/stop-tenants-example.sh
+++ scripts/stop-tenants-example.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-docker compose -f docker-compose.yml -f docker-compose.prod.yml \
+cd "$(dirname "$0")/.."
+docker compose --env-file .env.prod -f docker-compose.yml -f docker-compose.prod.yml \
     -f docker-compose.tenants.prod.yml down
